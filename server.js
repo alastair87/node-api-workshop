e8d6de6eeba16250ba2193777ef940206a83c455
Alastair Cooper <alastair.cooper@outlook.com>
2018-06-16 15:19:28 +0100

    Configured nodemon

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+// req is the Request object, res is the Response object
+// (these are just variable names, they can be anything but it's a convention to call them req and res)
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
